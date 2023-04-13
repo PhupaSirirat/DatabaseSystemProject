a347a1c4fa5d54fc3b136eb237ed368ba582e28c
Slyt4il <82972166+Slyt4il@users.noreply.github.com>
2023-04-14 00:31:13 +0700

    Try to fix unused var error

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React, { useState, useEffect } from 'react';
+//import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 function App() {
   
   <div>You should see this unless visiting /listUsers</div>
   
-  //const [accounts, setAccountState] = useState(false);
-  //useEffect(() => {
-    //getAllAccounts();
-  //}, []);
+  /* const [accounts, setAccountState] = useState(false);
+  useEffect(() => {
+    getAllAccounts();
+  }, []);
 
-  //function getAllAccounts() {
-    //fetch('https://workable-drink-production.up.railway.app/listUsers')
-      //.then(response => {
-        //console.log('connected');
-        //return response.text();
-      //})
-      //.then(data => {
-        //setAccountState(data);
-      //})
-  //}
+  function getAllAccounts() {
+    fetch('https://workable-drink-production.up.railway.app/listUsers')
+      .then(response => {
+        console.log('connected');
+        return response.text();
+      })
+      .then(data => {
+        setAccountState(data);
+      })
+  }
 
-  //return (
-    //<div>
-      //{/* {merchants ? merchants : 'There is no merchant data available'} */}
-      //{accounts ? accounts : 'There is no accounts data'}
+  return (
+    <div>
+      {accounts ? accounts : 'There is no accounts data'}
 
-    //</div>
-  //);
+    </div>
+  ); */
 }
 export default App;

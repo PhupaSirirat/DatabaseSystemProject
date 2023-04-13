7387eae4a19b834b3e2ab94b3c0748b73c55c42a
Slyt4il <82972166+Slyt4il@users.noreply.github.com>
2023-04-14 00:18:06 +0700

    Test

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
 function App() {
+  
+  //const [accounts, setAccountState] = useState(false);
+  //useEffect(() => {
+    //getAllAccounts();
+  //}, []);
 
-  const [accounts, setAccountState] = useState(false);
-  useEffect(() => {
-    getAllAccounts();
-  }, []);
+  //function getAllAccounts() {
+      return ( <div>Hello World</div> );
+    //fetch('https://workable-drink-production.up.railway.app/listUsers')
+      //.then(response => {
+        //console.log('connected');
+        //return response.text();
+      //})
+      //.then(data => {
+        //setAccountState(data);
+      //})
+  //}
 
-  function getAllAccounts() {
-    fetch('https://workable-drink-production.up.railway.app/listUsers')
-      .then(response => {
-        console.log('connected');
-        return response.text();
-      })
-      .then(data => {
-        setAccountState(data);
-      })
-  }
+  //return (
+    //<div>
+      //{/* {merchants ? merchants : 'There is no merchant data available'} */}
+      //{accounts ? accounts : 'There is no accounts data'}
 
-  return (
-    <div>
-      {/* {merchants ? merchants : 'There is no merchant data available'} */}
-      {accounts ? accounts : 'There is no accounts data'}
-
-    </div>
-  );
+    //</div>
+  //);
 }
 export default App;

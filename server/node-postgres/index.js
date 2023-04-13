f9c33f54333bcb444151e2e5dd56d6545183d8e4
Slyt4il <82972166+Slyt4il@users.noreply.github.com>
2023-04-14 01:20:27 +0700

    Configure ports

--- server/node-postgres/index.js
+++ server/node-postgres/index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = process.env.PORT || 3001
 
 const db = require('./database')
 
 app.use(express.json())
 app.use(function (req, res, next) {
-    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { useState, useEffect } from 'react';
 
 function App() {
   
+  const FETCH_TARGET = "http://localhost";
+  const FETCH_PORT = process.env.PORT || 3001;
+
   const [accounts, setAccountState] = useState(false);
   useEffect(() => {
     getAllAccounts();
   }, []);
 
   function getAllAccounts() {
-    fetch('http://localhost:3001')
+    fetch(FETCH_TARGET.concat(FETCH_PORT))
       .then(response => {
         console.log('connected');
         return response.text();

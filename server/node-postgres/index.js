7e80c149b6ac6ca9ec6fcdaeb9fc272c1aba5c50
Slyt4il <82972166+Slyt4il@users.noreply.github.com>
2023-04-14 15:52:20 +0700

    ✨ Add /api/sql-query endpoint

--- server/node-postgres/index.js
+++ server/node-postgres/index.js
@@ -13,11 +13,11 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', async (req, res) => {
-    res.send({message: 'If you see this when visiting the landing page. Then it works! OTHER END POINTS: /listUsers should return all accounts. You can filter results with ?<field>&<value>= ex: /listUsers?field=username&value=user3 returns account with username of user3'})
+    res.send({message: 'If you see this when visiting the landing page. Then it works! Documentation is available on Github - https://github.com/PhupaSirirat/DatabaseSystemProject'})
 })
 
 // Get all data in the {param} table
-app.get('/getData', async (req, res) => {
+app.get('/api/get-data', async (req, res) => {
     const params = new URLSearchParams(req.query);
     let sql = '';
     if (params.get('table') && !params.get('field') && !params.get('value')) {
@@ -28,7 +28,18 @@ app.get('/getData', async (req, res) => {
     }
     const result = await db.query(sql);
     res.json(result);
-  });  // !!VULNERABLE TO SQL INJECTION!!
+  });
+  
+  app.get('/api/sql-query', async (req, res) => {
+    let sql = new URLSearchParams(req.query).toString();
+    if (!sql) {
+      return;
+    }
+    const result = await db.query(sql);
+    res.json(result);
+  });
+  
+  // !!VULNERABLE TO SQL INJECTION!!
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)

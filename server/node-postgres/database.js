d46011de53759189489e5067fb733ba4da866123
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2023-04-15 06:55:28 +0700

    Comment out pool variables

--- server/node-postgres/database.js
+++ server/node-postgres/database.js
@@ -1,21 +1,24 @@
+require('dotenv').config(); // Load environment variables from .env file
+
 const { Pool } = require('pg');
 
 // Create a new Pool instance with your PostgreSQL database connection details
-// const pool = new Pool({
-//   user: process.env.PGUSER,
-//   password: process.env.PGPASSWORD,
-//   host: process.env.PGHOST,
-//   port: process.env.PGPORT,
-//   database: process.env.PGDATABASE,
-// });
-
 const pool = new Pool({
-  user: 'postgres',
-  password: 'XfaaDvXn4aYqjM8xDfy9',
-  host: 'containers-us-west-3.railway.app',
-  port: '6789',
-  database: 'railway',
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
 });
+
+// Connect postgres from railway
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: 'XfaaDvXn4aYqjM8xDfy9',
+//   host: 'containers-us-west-3.railway.app',
+//   port: '6789',
+//   database: 'railway',
+// });
 
 // Function to run a query using the pool
 const query = async (sql, values) => {

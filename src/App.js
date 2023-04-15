d32ba2d7c02c5e683434aee00f6d3016715189f3
KanbH <pohbnak@gmail.com>
2023-04-16 00:07:54 +0700

    Revert "Temp Allgames css"
    This reverts commit a5c010ab4dbb46b6a25dd9c3b896862826f25ac8.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './Style/GameCard.css';
 import { Link } from 'react-router-dom';
+import './Style/GameCard.css';
 import axios from 'axios';
-import './Style/AllGames.css';
 
 function App() {
   const [data, setData] = useState([]); // Initialize state for accounts data
@@ -37,41 +37,41 @@ function App() {
       <div className='title'>
         <h1>All Game Page</h1>
       </div>
+      <hr></hr>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="sql">Execute SQL query:</label>
+        <input type="text" id="sql" name="sql" value={sql} onChange={(e) => setSql(e.target.value)} />
+        <button type="submit">Submit</button>
+      </form>
+
       <Link to={"/addgame"}>
         <button className='nice_butt_on'>Add Game</button>
       </Link>
+
       <Link to={"/accounts"}>
         <button className='nice_dark_butt_on'>Accounts</button>
       </Link>
-      <div className='querybox'>
-          <form onSubmit={handleSubmit}>
-            <label htmlFor="sql">Execute SQL query:</label>
-            <input type="text" id="sql" name="sql" value={sql} onChange={(e) => setSql(e.target.value)} />
-            <button type="submit">Submit</button>
-          </form>
-        </div>
-      <hr></hr>
-      <div className='resultcontain'>
-        
-          {data.length > 0 ? (
-            // Render data if available
-            data.map(item => (
-              <Link to={`/game-detail/${item.gameid}`}>
-                <div key={item.gameid} className="GameCard" style={{backgroundImage: item.thumbnail_link}}>
-                  <p>Game: {item.gamename}</p>
-                  {/* <p>GameID: {item.gameid}</p> */}
-                  <p>Genre: {item.genre}</p>
-                  <p>Version: {item.version}</p>
-                  {/* <p>Desc: {item.description}</p> */}
-                  {/* <p>Release Date: {item.releasedate}</p> */}
-                  {/* <p>Systems: {item.systems}</p> */}
-                </div>
-              </Link>
-            ))
-          ) : (
-            // Render message if no data available
-            <p>Data fetching...</p>
-          )}
+
+      <div className="allGames">
+        {data.length > 0 ? (
+          // Render data if available
+          data.map(item => (
+            <Link to={`/game-detail/${item.gameid}`}>
+              <div key={item.gameid} className="GameCard">
+                <p>Game: {item.gamename}</p>
+                {/* <p>GameID: {item.gameid}</p> */}
+                <p>Genre: {item.genre}</p>
+                <p>Version: {item.version}</p>
+                {/* <p>Desc: {item.description}</p> */}
+                {/* <p>Release Date: {item.releasedate}</p> */}
+                {/* <p>Systems: {item.systems}</p> */}
+              </div>
+            </Link>
+          ))
+        ) : (
+          // Render message if no data available
+          <p>Data fetching...</p>
+        )}
       </div>
     </main>
   );

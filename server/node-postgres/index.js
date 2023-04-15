c0f24f4408d280e65a6b435daf00aee30008c333
Slyt4il <82972166+Slyt4il@users.noreply.github.com>
2023-04-15 13:03:10 +0700

    :sparkles: New endpoint for sql insertion

--- server/node-postgres/index.js
+++ server/node-postgres/index.js
@@ -32,21 +32,56 @@ app.get('/api/get-data', async (req, res) => {
     const result = await db.query(sql);
     res.json(result);
   });
- 
-  app.get('/api/query', async (req, res) => {
-    const params = new URLSearchParams(req.query);
-    let sql = '';
-    if (params.get('sql')) {
-      sql = params.get('sql').replace(/\+/g, ' ');
-      sql = sql.replace(/\-/g, '=');
-      sql = sql.replace(/\./g, "'");
-    }
-    if (!sql) {
-      return;
-    }
-    const result = await db.query(sql);
-    res.json(result);
-  });
+
+app.get('/api/post-data', async (req, res) => {
+  const params = new URLSearchParams(req.query);
+  let sql = '';
+  if (params.get('table') && params.get('columns') && params.get('values')) {
+    let new_cols = params.get('columns').replace(/\~/g, ', ')
+    new_cols = new_cols.replace(/\./g, "'")
+    new_cols = new_cols.replace(/\+/g, " ")
+    let new_values = params.get('values').replace(/\~/g, ', ')
+    new_values = new_values.replace(/\./g, "'")
+    new_values = new_values.replace(/\+/g, " ")
+    sql = `INSERT INTO ${params.get('table')} (${new_cols}) VALUES (${new_values})`
+  }
+  if (!sql) {
+    return;
+  }
+  const result = await db.query(sql);
+  res.json(result);
+});
+
+app.get('/api/post-data-fill', async (req, res) => {
+  const params = new URLSearchParams(req.query);
+  let sql = '';
+  if (params.get('table') && params.get('values')) {
+    let new_values = params.get('values').replace(/\~/g, ', ')
+    new_values = new_values.replace(/\./g, "'")
+    new_values = new_values.replace(/\+/g, " ")
+    sql = `INSERT INTO ${params.get('table')} VALUES (${new_cols})`
+  }
+  if (!sql) {
+    return;
+  }
+  const result = await db.query(sql);
+  res.json(result);
+});
+
+app.get('/api/query', async (req, res) => {
+  const params = new URLSearchParams(req.query);
+  let sql = '';
+  if (params.get('sql')) {
+    sql = params.get('sql').replace(/\+/g, ' ');
+    sql = sql.replace(/\-/g, '=');
+    sql = sql.replace(/\./g, "'");
+  }
+  if (!sql) {
+    return;
+  }
+  const result = await db.query(sql);
+  res.json(result);
+});
   
   // !!VULNERABLE TO SQL INJECTION!!
 

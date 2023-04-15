eca265fe53ad1177174d8f8521ea62f7dbb29c57
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2023-04-15 23:06:26 +0700

    Update Router.js

--- src/Route/Router.js
+++ src/Route/Router.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from '../App';
-import GameDetail from '../GameDetail';
-import AddGame from '../components/AddGame';
-import EditGame from '../components/EditGame';
+import GameDetail from '../components/Detail/GameDetail';
+import AddGame from '../components/Create/AddGame';
+import EditGame from '../components/Edit/EditGame';
+import ServerDetail from '../components/Detail/ServerDetail';
+import TopPlayer from '../components/TopPlayer';
+import PlayerDetail from '../components/Detail/PlayerDetail';
+import Accounts from '../components/Accounts';
+import CreateAcc from '../components/Create/CreateAcc';
+import AccountDetail from '../components/Detail/AccountDetail';
 
 const Routers = () => {
     return (
@@ -14,6 +20,12 @@ const Routers = () => {
                 <Route path="/game-detail/:slug" element={<GameDetail />}/>
                 <Route path="/addgame" element={<AddGame/>}/>
                 <Route path="/editgame/:slug" element={<EditGame/>}/>
+                <Route path="/server-detail/:slug" element={<ServerDetail />}/>
+                <Route path="/game-detail/:slug/top-player" element={<TopPlayer />}/>
+                <Route path="/game-detail/:gameid/player-detail/:slug" element={<PlayerDetail />}/>
+                <Route path="/accounts" element={<Accounts />}/>
+                <Route path="/accounts/create-account" element={<CreateAcc />}/>
+                <Route path="/accounts/account-detail/:slug" element={<AccountDetail />}/>
             </Routes>
         </BrowserRouter>
     )

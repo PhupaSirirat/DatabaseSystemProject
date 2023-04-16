24110cf852459a4843ecf211b9262606505d0610
Slyt4il <82972166+Slyt4il@users.noreply.github.com>
2023-04-16 13:33:04 +0700

    :rotating_light: Disable the ESLint rule that requires all dependencies to be listed

--- src/components/Edit/EditServer.js
+++ src/components/Edit/EditServer.js
@@ -10,6 +10,7 @@ export default function EditServer() {
     useEffect(() => {
         fetchServer();
         fetchServerLocation();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const fetchServer = () => {

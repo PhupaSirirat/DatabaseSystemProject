dfb49dbfbba14f97f664794bd64515aa455da815
Phupa S <99344705+PhupaSirirat@users.noreply.github.com>
2023-04-27 23:45:31 +0700

    Link update again

--- src/components/header.js
+++ src/components/header.js
@@ -4,11 +4,10 @@ import { Link } from 'react-router-dom';
 export default function header() {
     return (
         <header className="header">
-            <Link href="/home">
+            <Link to="/home">
                 <img src="/icons/home.png" alt="home" className="home-btn"/>
                 <p className="header-p">Game Management System</p>
             </Link>
-            
         </header>
     )
 }
